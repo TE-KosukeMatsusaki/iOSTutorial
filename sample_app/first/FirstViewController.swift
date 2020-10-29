3950f52cc73e422dca96194659144f410e90e27c
TE-KosukeMatsusaki <kosuke.matsusaki@sharing-innovations.co.jp>
2020-10-29 19:12:46 +0900

    refactor: remove unnecessary parts

--- sample_app/first/FirstViewController.swift
+++ sample_app/first/FirstViewController.swift
@@ -22,14 +22,4 @@ class FirstViewController: UIViewController {
     @objc func showSecondViewController() {
         navigationController?.pushViewController(SecondViewController(), animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- sample_app/main/MainViewController.swift
+++ sample_app/main/MainViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var secondButton: UIButton!
     
     override func viewDidLoad() {
@@ -34,19 +33,6 @@ class MainViewController: UIViewController {
     }
     
     @IBAction func clickFirstButton(_ sender: Any) {
-        mainLabel.text = "Move to First"
         showFirstViewController()
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
