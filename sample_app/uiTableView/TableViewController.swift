ef027812c4dbc253ecc3c4388481b3adab86fb19
TE-KosukeMatsusaki <kosuke.matsusaki@sharing-innovations.co.jp>
2020-11-02 13:16:27 +0900

    feat: add tap action

--- sample_app/uiTableView/TableViewController.swift
+++ sample_app/uiTableView/TableViewController.swift
@@ -22,25 +22,27 @@ class TableViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        title = "日本昔話一覧"
     }
 
 }
 
 extension TableViewController: UITableViewDataSource {
-     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
-     }
+    }
 
-     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(TableViewCell.self), for: indexPath)
         cell.textLabel?.text = items[indexPath.row]
         return cell
-     }
+    }
 }
 
 extension TableViewController: UITableViewDelegate {
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        navigationController?.pushViewController(ScrollViewController(), animated: true)
+    }
 }
